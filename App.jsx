a8739ca70a87b350255823a08d0bbab9077e8b94
Dennis Tekell <dennistekell@berkeley.edu>
2019-02-18 19:31:49 -0800

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+// React frontend
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>simon, helloworld!!!</div>
+    );
+  }
+}
+
+export default App;
